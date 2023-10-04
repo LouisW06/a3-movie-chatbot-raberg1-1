ff0b1b80b649092340e74073f27c8877839d7414
Robert Berg <raberg1@gmail.com>
2023-10-04 11:27:15 -0500

    finished a3

--- a3.py
+++ a3.py
@@ -253,7 +253,13 @@ def search_pa_list(src: List[str]) -> List[str]:
         a list of answers. Will be ["I don't understand"] if it finds no matches and
         ["No answers"] if it finds a match but no answers
     """
-    pass
+    for pat, act in pa_list:
+        mat = match(pat, src)
+        if mat is not None:
+            answer = act(mat)
+            # print(answer)
+            return answer if answer else ["No answers"]   
+    return ["I don't understand"]
 
 
 def query_loop() -> None:
@@ -278,7 +284,7 @@ def query_loop() -> None:
 # uncomment the following line once you've written all of your code and are ready to try
 # it out. Before running the following line, you should make sure that your code passes
 # the existing asserts.
-# query_loop()
+query_loop()
 
 if __name__ == "__main__":
     assert isinstance(title_by_year(["1974"]), list), "title_by_year not returning a list"
@@ -325,14 +331,14 @@ if __name__ == "__main__":
     assert sorted(title_by_actor(["orson welles"])) == sorted(
         ["citizen kane", "othello"]
     ), "failed title_by_actor test"
-    # assert sorted(search_pa_list(["hi", "there"])) == sorted(
-    #     ["I don't understand"]
-    # ), "failed search_pa_list test 1"
-    # assert sorted(search_pa_list(["who", "directed", "jaws"])) == sorted(
-    #     ["steven spielberg"]
-    # ), "failed search_pa_list test 2"
-    # assert sorted(
-    #     search_pa_list(["what", "movies", "were", "made", "in", "2020"])
-    # ) == sorted(["No answers"]), "failed search_pa_list test 3"
+    assert sorted(search_pa_list(["hi", "there"])) == sorted(
+        ["I don't understand"]
+    ), "failed search_pa_list test 1"
+    assert sorted(search_pa_list(["who", "directed", "jaws"])) == sorted(
+        ["steven spielberg"]
+    ), "failed search_pa_list test 2"
+    assert sorted(
+        search_pa_list(["what", "movies", "were", "made", "in", "2020"])
+    ) == sorted(["No answers"]), "failed search_pa_list test 3"
 
     print("All tests passed!")
